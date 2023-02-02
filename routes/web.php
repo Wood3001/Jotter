736c8256f9a1dba33b5f0f91ab1195ea0287f7a1
Wood3001 <wood3001@gmail.com>
2023-02-02 14:44:45 -0800

    Add initial commit

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TrashedNoteController;
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('/notes', NoteController::class)->middleware(['auth']);
+
+Route::get('/trashed', [TrashedNoteController::class, 'index'])->middleware('auth')->name('trashed.index');
+
+Route::get('/trashed/{note}', [TrashedNoteController::class, 'show'])
+        ->withTrashed()
+        ->middleware('auth')->name('trashed.show');
+
+Route::put('/trashed/{note}', [TrashedNoteController::class, 'update'])
+        ->withTrashed()
+        ->middleware('auth')->name('trashed.update');
+
+Route::delete('/trashed/{note}', [TrashedNoteController::class, 'destroy'])
+        ->withTrashed()
+        ->middleware('auth')->name('trashed.destroy');
+
+// Route::prefix('/trashed')->name('trashed.')->middleware('auth')->group(function(){
+//     Route::get('/', [TrashedNoteController::class, 'index'])->name('index');
+//     Route::get('/{note}', [TrashedNoteController::class, 'show'])->name('show')->withTrashed();
+//     Route::put('/{note}', [TrashedNoteController::class, 'update'])->name('index')->withTrashed();
+//     Route::delete('/{note}', [TrashedNoteController::class, 'destroy'])->name('index')->withTrashed();
+// });
+
+require __DIR__.'/auth.php';
