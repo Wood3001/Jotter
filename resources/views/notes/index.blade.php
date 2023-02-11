84213b713c006a0cba3159960919fa4bc8b27876
Wood3001 <wood3001@gmail.com>
2023-02-10 21:46:44 -0800

    fixed pagination issue
    stopped 'Search Results' heading from appearing when the pagination links were clicked

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             @if(request()->routeIs('notes.index'))
-                @if(request()->query())
+                @if(request()->query('term'))
                     {{ __('Search Results')  }}
                 @else
                     {{ __('Notes')  }}
