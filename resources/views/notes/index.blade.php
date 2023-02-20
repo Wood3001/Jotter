12fc5c44efd98d3087ac2ef6f568e4abe9f9cd3b
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-02-20 13:47:38 -0800

    button style change & added hover effect on note titles

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -24,7 +24,7 @@
 
             @forelse ($notes as $note)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                    <h2 class="font-bold text-2xl">
+                    <h2 class="font-bold text-2xl note-title">
                         <a 
                         @if(request()->routeIs('notes.index'))
                             href="{{ route('notes.show', $note) }}"
